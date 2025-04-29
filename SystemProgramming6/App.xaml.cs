07518f40f16325ca684e49b2367fcf6ad08552f4
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-04-29 20:01:32 +0300

    ex. 4 had done

--- SystemProgramming6/App.xaml.cs
+++ SystemProgramming6/App.xaml.cs
@@ -1,12 +1,22 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace SystemProgramming6;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    private static Mutex mutex;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        bool isNewInstance;
+        mutex = new Mutex(true, "SystemProgramming6Mutex", out isNewInstance);
+
+        if (!isNewInstance)
+        {
+            MessageBox.Show("The application is already running!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            Shutdown();
+        }
+    }
 }
--- SystemProgramming6/MainWindow.xaml.cs
+++ SystemProgramming6/MainWindow.xaml.cs
@@ -1,53 +1,15 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
 
-namespace SystemProgramming6;
+namespace SystemProgramming6
+{
 
-
-public partial class MainWindow : Window
-{private static Mutex mutex = new Mutex();
-    private static TextBlock outputText;
-
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-        outputText = OutputText;
-    }
-
-    private void StartThreads_Click(object sender, RoutedEventArgs e)
-    {
-        new Thread(ShowAscendingNumbers).Start();
-        new Thread(ShowDescendingNumbers).Start();
-    }
-
-    private void ShowAscendingNumbers()
-    {
-        mutex.WaitOne();
-        for (int i = 0; i <= 20; i++)
+        public MainWindow()
         {
-            UpdateUI($"Ascending: {i}");
-            Thread.Sleep(500);
+            InitializeComponent();
         }
-        mutex.ReleaseMutex();
-    }
-
-    private void ShowDescendingNumbers()
-    {
-        mutex.WaitOne();
-        for (int i = 10; i >= 0; i--)
-        {
-            UpdateUI($"Descending: {i}");
-            Thread.Sleep(500);
-        }
-        mutex.ReleaseMutex();
-    }
-
-    private void UpdateUI(string text)
-    {
-        Dispatcher.Invoke(() =>
-        {
-            outputText.Text += text + "\n";
-        });
     }
 }
