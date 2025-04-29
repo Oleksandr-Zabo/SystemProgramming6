5911ac00d98fb79dcf5ad604a57967757a191e86
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-04-29 19:33:27 +0300

    ex. 2 had done

--- SystemProgramming6/MainWindow.xaml.cs
+++ SystemProgramming6/MainWindow.xaml.cs
@@ -6,48 +6,64 @@ namespace SystemProgramming6;
 
 
 public partial class MainWindow : Window
-{private static Mutex mutex = new Mutex();
-    private static TextBlock outputText;
+{
+    private static Mutex _mutex = new Mutex();
+    private static int[] _dataArray = Enumerable.Range(1, 10).ToArray();
+    private static Random _random = new Random();
+    private static TextBlock _outputText;
 
     public MainWindow()
     {
         InitializeComponent();
-        outputText = OutputText;
+        _outputText = OutputText;
     }
 
     private void StartThreads_Click(object sender, RoutedEventArgs e)
     {
-        new Thread(ShowAscendingNumbers).Start();
-        new Thread(ShowDescendingNumbers).Start();
+        new Thread(WriteArray).Start();
+        new Thread(ModifyArray).Start();
+        new Thread(FindMaxValue).Start();
     }
 
-    private void ShowAscendingNumbers()
+    private void WriteArray()
     {
-        mutex.WaitOne();
-        for (int i = 0; i <= 20; i++)
+        _mutex.WaitOne();
+        string array = "Array";
+        foreach (var el in _dataArray)
         {
-            UpdateUI($"Ascending: {i}");
+            array += " " + el;
+        }
+        UpdateUi(array);
+        Thread.Sleep(500);
+        _mutex.ReleaseMutex();
+    }
+
+    private void ModifyArray()
+    {
+        _mutex.WaitOne();
+        for (int i = 0; i < _dataArray.Length; i++)
+        {
+            int randomValue = _random.Next(1, 10);
+            _dataArray[i] += randomValue;
+            UpdateUi($"Modified: [{i}] - {_dataArray[i]}");
             Thread.Sleep(500);
         }
-        mutex.ReleaseMutex();
+        _mutex.ReleaseMutex();
     }
 
-    private void ShowDescendingNumbers()
+    private void FindMaxValue()
     {
-        mutex.WaitOne();
-        for (int i = 10; i >= 0; i--)
-        {
-            UpdateUI($"Descending: {i}");
-            Thread.Sleep(500);
-        }
-        mutex.ReleaseMutex();
+        _mutex.WaitOne();
+        int maxValue = _dataArray.Max();
+        UpdateUi($"Max Value: {maxValue}");
+        _mutex.ReleaseMutex();
     }
 
-    private void UpdateUI(string text)
+    private void UpdateUi(string text)
     {
         Dispatcher.Invoke(() =>
         {
-            outputText.Text += text + "\n";
+            _outputText.Text += text + "\n";
         });
     }
 }
